54d9fe37fd5b10ed2a072b1f40c95536a171568e
Denis Dezest <denis.dezest@gmail.com>
2025-08-31 11:00:43 +0200

    🎉 Init repo.

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <h1>Your app...</h1>
+}
+
+export default App
---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+import { defineConfig } from 'vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@/components': path.resolve(__dirname, './src/components/*'),
+      '@/features/*': path.resolve(__dirname, './src/features/*'),
+      '@/shared/*': path.resolve(__dirname, './src/shared/*'),
+      '@/lib/*': path.resolve(__dirname, './src/lib'),
+      '@/*': path.resolve(__dirname, './src/*'),
+    },
+  },
+})
